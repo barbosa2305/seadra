7b1e0723ebd6e3c575d24d57cc5739b9be01a368
Jose Barbosa de Macedo Junior <jose.barbosa@mpdft.mp.br>
2019-01-30 15:00:39 -0200

    Nova configuração.

--- modulos/gerasenha.php
+++ modulos/gerasenha.php
@@ -8,3 +8,6 @@ echo 'admin123: ' . $pwd_user_new_hash. '<br/>';
 
 $pwd_user_new_hash = password_hash('12345678', PASSWORD_DEFAULT);
 echo '12345678: ' . $pwd_user_new_hash . '<br/>';
+
+$pwd_user_new_hash = password_hash('@seadraadm', PASSWORD_DEFAULT);
+echo '@seadraadm: ' . $pwd_user_new_hash . '<br/>';
