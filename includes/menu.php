76abf12640f67f82cfbc1fb005cdd6ec098f804c
Jose Barbosa de Macedo Junior <jose.barbosa@mpdft.mp.br>
2019-02-01 21:05:20 -0200

    Remoção do menu gerador interno.

--- includes/menu.php
+++ includes/menu.php
@@ -21,7 +21,7 @@ $menu->add('2', null, 'Acesso', null, null, 'icon-key-yellow.png');
 $menu->add('2.1',2,'Alterar Minha Senha','modulos/alterasenha.php', null, 'lock16.gif');
 $menu->add('2.2',2,'Usuário','modulos/usuario.php');
 
-$menu->add('9', null, 'Sobre', 'modulos/sys_about.php', null, 'information-circle.jpg');
+$menu->add('9', null, 'Sobre', 'modulos/sys_about.php', null, 'information16.gif');
 
 if (Acesso::isUserAdm()) {
     $menu->add('1.3',1,'itempedido','modulos/itempedido.php');
@@ -30,10 +30,8 @@ if (Acesso::isUserAdm()) {
     $menu->add('1.6',1,'unidadefederativa','modulos/unidadefederativa.php');
 
     $menu->add('10',null,'Config Ambiente',null,null,'setting-gear-512.png');
-    $menu->add('10.1','10','Ambiente Resumido','modulos/sys_environment_summary.php',null,'information-circle.jpg');
+    $menu->add('10.1','10','Ambiente Resumido','modulos/sys_environment_summary.php',null,'information16.gif');
     $menu->add('10.2','10','PHPInfo','modulos/sys_environment.php',null,'php_logo.png');
-    $menu->add('10.4','10','Gerador VO/DAO','../base/includes/gerador_vo_dao.php');
-    $menu->add('10.5','10','Gerador Form VO/DAO','../base/includes/gerador_form_vo_dao.php',null,'smiley-1-512.png');
 }
 
 $menu->getXml();
