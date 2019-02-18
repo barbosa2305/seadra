f2955ce6244e5a40a6723cb94a938ffa6757650e
Jose Barbosa Jr <barbosa2305@gmail.com>
2019-02-17 22:09:33 -0300

    Adição de novas mensagens.

--- classes/Mensagem.class.php
+++ classes/Mensagem.class.php
@@ -11,8 +11,11 @@ final class Mensagem {
     const OPERACAO_COM_SUCESSO = 'Operação realizada com sucesso.';
     
     const USUARIO_SENHA_INCORRETOS = 'Usuário e/ou senha incorretos.';
+    const USUARIO_JA_CADASTRADO = 'O usuário já está cadastrado.';
     const SENHA_TAMANHO_MINIMO = 'A senha deve ter no mínimo 8 caracteres.';
     const SENHAS_NAO_COINCIDEM = 'As senhas não conferem.';
     const SENHA_ATUAL_INCORRETA = 'A senha atual não está correta.';    
     const SENHA_PADRAO_USUARIO = 'A senha padrão: 12345678, foi definida para este usuário.';
+
+    const CPFCNPJ_JA_CADASTRADO = 'O CPF/CNPJ informado já está cadastrado.';
 }
