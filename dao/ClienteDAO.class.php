db725d506e755fba4ea0c77f27740de825fbc9cb
Jose Barbosa Jr <barbosa2305@gmail.com>
2019-02-20 10:16:50 -0300

    Trim nas strings antes de gravar.

--- dao/ClienteDAO.class.php
+++ dao/ClienteDAO.class.php
@@ -23,7 +23,7 @@ class ClienteDAO extends TPDOConnection {
 									 ,dsunidadefederativa
 									 from seadra.vw_cliente ';
 
-	private static function processWhereGridParameters( $whereGrid ) {
+	private static function processWhereGridParameters( $whereGrid ){
 		$result = $whereGrid;
 		if ( is_array($whereGrid) ){
 			$where = ' 1=1 ';
@@ -50,48 +50,42 @@ class ClienteDAO extends TPDOConnection {
 		return $result;
 	}
 	//--------------------------------------------------------------------------------
-	public static function selectById( $id ) {
-		$values = array($id);
+	public static function selectById( $id ){
+		$values = array( $id );
 		$sql = self::$sqlBasicSelect.' where idCliente = ?';
-		$result = self::executeSql($sql, $values );
-		return $result;
+		return self::executeSql( $sql,$values );
 	}
 	//--------------------------------------------------------------------------------
 	public static function selectCount( $where=null ){
-		$where = self::processWhereGridParameters($where);
+		$where = self::processWhereGridParameters( $where );
 		$sql = 'select count(idCliente) as qtd from seadra.vw_cliente';
 		$sql = $sql.( ($where)? ' where '.$where:'');
-		$result = self::executeSql($sql);
+		$result = self::executeSql( $sql );
 		return $result['QTD'][0];
 	}
 	//--------------------------------------------------------------------------------
-	public static function selectAllPagination( $orderBy=null, $where=null, $page=null,  $rowsPerPage= null ) {
-		$rowStart = PaginationSQLHelper::getRowStart($page,$rowsPerPage);
-		$where = self::processWhereGridParameters($where);
-
+	public static function selectAllPagination( $orderBy=null,$where=null,$page=null,$rowsPerPage=null ){
+		$rowStart = PaginationSQLHelper::getRowStart( $page,$rowsPerPage );
+		$where = self::processWhereGridParameters( $where );
 		$sql = self::$sqlBasicSelect
 		.( ($where)? ' where '.$where:'')
 		.( ($orderBy) ? ' order by '.$orderBy:'')
 		.( ' LIMIT '.$rowStart.','.$rowsPerPage);
-
-		$result = self::executeSql($sql);
-		return $result;
+		return self::executeSql( $sql );
 	}
 	//--------------------------------------------------------------------------------
-	public static function selectAll( $orderBy=null, $where=null ) {
-		$where = self::processWhereGridParameters($where);
+	public static function selectAll( $orderBy=null,$where=null ){
+		$where = self::processWhereGridParameters( $where );
 		$sql = self::$sqlBasicSelect
 		.( ($where)? ' where '.$where:'')
 		.( ($orderBy) ? ' order by '.$orderBy:'');
-
-		$result = self::executeSql($sql);
-		return $result;
+		return self::executeSql( $sql );
 	}
 	//--------------------------------------------------------------------------------
-	public static function insert( ClienteVO $objVo ) {
-		$values = array(  $objVo->getNmcliente() 
+	public static function insert( ClienteVO $objVo ){
+		$values = array(  trim($objVo->getNmcliente())
 						, $objVo->getNrcpfcnpj() 
-						, $objVo->getDsemail() 
+						, trim($objVo->getDsemail())
 						, $objVo->getNrtelefone() 
 						, $objVo->getNrcelular() 
 						, $objVo->getIdusuario()
@@ -107,10 +101,10 @@ class ClienteDAO extends TPDOConnection {
 		return self::getLastId('seadra.cliente','idCliente');
 	}
 	//--------------------------------------------------------------------------------
-	public static function update ( ClienteVO $objVo ) {
-		$values = array( $objVo->getNmcliente()
+	public static function update ( ClienteVO $objVo ){
+		$values = array( trim($objVo->getNmcliente())
 						,$objVo->getNrcpfcnpj()
-						,$objVo->getDsemail()
+						,trim($objVo->getDsemail())
 						,$objVo->getNrtelefone()
 						,$objVo->getNrcelular()
 						,$objVo->getStativo()
@@ -127,7 +121,7 @@ class ClienteDAO extends TPDOConnection {
 								where idCliente = ?',$values);
 	}
 	//--------------------------------------------------------------------------------
-	public static function updateStatus ( ClienteVO $objVo ) {
+	public static function updateStatus ( ClienteVO $objVo ){
 		$values = array( $objVo->getStativo()
 						,$objVo->getIdCliente() );
 		return self::executeSql('update seadra.cliente set 
