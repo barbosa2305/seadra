8ddd79775814b1c57e6ed443939355e35599bb74
Jose Barbosa de Macedo Junior <jose.barbosa@mpdft.mp.br>
2019-03-13 15:38:19 -0300

    Importação de dados - PREVIA

--- classes/ImportaDados.class.php
+++ classes/ImportaDados.class.php
@@ -2,6 +2,9 @@
 
 class ImportaDados {
 
+    const TIPO_ARQUIVO_CLIENTE = 'C';
+    const TIPO_ARQUIVO_PRODUTO = 'P';
+
 	public function __construct(){
 	}
 	//--------------------------------------------------------------------------------
@@ -13,30 +16,54 @@ class ImportaDados {
 		return $result;
 	}
   //--------------------------------------------------------------------------------
-	public static function importa( $infoArquivo,$tipo,$dados ){
+	public static function importa( $tipo,$infoArquivo ){
 		$result = null;
-		//d($tipo);
-		//d($dados);
-		//d($infoArquivo);
+        $arquivo = $infoArquivo['arquivo_temp_name'];
+        if ( $tipo == self::TIPO_ARQUIVO_CLIENTE ) {
+            self::importaCliente( $arquivo );
+        } elseif ( $tipo == self::TIPO_ARQUIVO_PRODUTO ) {
+            self::importaProduto( $arquivo );
+        }
+        self::apagaArquivo( $arquivo );
+		return $result;
+    }  
+    //--------------------------------------------------------------------------------
+	private static function importaCliente( $arquivo ){
+		$result = null;
+		if ( ($objeto = fopen($arquivo,'r')) !== FALSE ) {
+            $linha = 1;
+            while( ($dados = fgetcsv($objeto,1000,';')) !== FALSE ) {
+                /*
+                $num = count($dados);
+                echo "<p> $num campos na linha $linha: <br /></p>\n";
+                */
+                $linha++;
+                /*
+                for ($c=0; $c < $num; $c++) {
+                    echo utf8_encode($dados[$c]) . "<br />\n";
+                }
+                */
 
-		$arquivo = $infoArquivo['ANEXO']['tmp_name'];
-		$nome = $infoArquivo['ANEXO']['name'];
-
-		$objeto = fopen($arquivo,"r");
-
-		while( ($linha = fgetcsv($objeto,1000,";")) !== FALSE ) {
-			$nome = utf8_encode($linha[0]);
-			$teste = utf8_encode($linha[1]);
-
-			echo $nome.'\n';
-			echo $teste.'\n';
-
-		}
-
+                //$nome = utf8_encode($linha[0]);
+            }
+            fclose($objeto);
+        }  
 
 		$result = TRUE;
 		return $result;
     }  
-	//--------------------------------------------------------------------------------
+    //--------------------------------------------------------------------------------
+	private static function importaProduto( $arquivo ){
+		$result = null;
+    
+
+		$result = TRUE;
+		return $result;
+    } 
+    //--------------------------------------------------------------------------------
+	private static function apagaArquivo( $arquivo ){
+		if ( file_exists($arquivo) ) unlink( $arquivo );
+    }  
+    //--------------------------------------------------------------------------------
 }
 ?>
--- modulos/importadados.php
+++ modulos/importadados.php
@@ -6,11 +6,21 @@ $frm = new TForm('Importar dados', 250, 550);
 $fileFormat = 'csv';
 $frm->setColumns('100,300');
 $frm->addRadioField('TIPODADO','Dados de: ',TRUE,'C=Clientes,P=Produtos',null,FALSE,null,null,null,null,null,FALSE);
-$frm->addHtmlField('html1', 'Arquivo de tamanho maximo de 2MB e no formato: '.$fileFormat, null, 'Dica:', null, 200)->setCss('border', '1px dashed blue');
-$frm->addFileField('ANEXO','Arquivo: ',TRUE,$fileFormat,'2M',40,FALSE);
+$frm->addHtmlField('html1', 'Arquivo de tamanho maximo de 5MB e no formato: '.$fileFormat, null, 'Dica:', null, 200)->setCss('border', '1px dashed blue');
+$arquivo = 'ARQUIVODADOS';
+$frm->addFileField($arquivo,'Arquivo: ',TRUE,$fileFormat,'5M',40,TRUE);
 
 $frm->addButton('Importar', null, 'Importar', null, null, TRUE, TRUE);
 $frm->addButton('Limpar', null, 'Limpar', null, null, FALSE, TRUE);
+
+function getPostArquivo(&$frm,$arquivo){
+    $resultado['arquivo_name']      = PostHelper::get($arquivo);
+    $resultado['arquivo_temp_name'] = $frm->getBase().PostHelper::get($arquivo.'_temp_name');
+    $resultado['arquivo_extension'] = PostHelper::get($arquivo.'_extension');
+    $resultado['arquivo_size']      = PostHelper::get($arquivo.'_size');
+    $resultado['arquivo_type']      = PostHelper::get($arquivo.'_type');
+    return $resultado;
+}
 
 $acao = isset($acao) ? $acao : null;
 switch ($acao) {
@@ -21,10 +31,10 @@ switch ($acao) {
     //--------------------------------------------------------------------------------
     case 'Importar': {
         try {
-			if ( $frm->validate() ){                
+			if ( $frm->validate() ){ 
+                $postArquivo = getPostArquivo( $frm,$arquivo );
                 $tipo = $frm->get('TIPODADO');
-                $dados = $frm->createBvars('ANEXO');
-                $resultado = ImportaDados::importa( $_FILES,$tipo,$dados );
+                $resultado = ImportaDados::importa( $tipo,$postArquivo );
 				if ( $resultado ){
 					$frm->setMessage( Mensagem::OPERACAO_COM_SUCESSO );
 					$frm->clearFields();
