d587fc9076def473cbd3cbb93b488803cdb0727e
Jose Barbosa Jr <barbosa2305@gmail.com>
2019-04-02 10:06:58 -0300

    Nova versão

--- ajuda/changelog.php
+++ ajuda/changelog.php
@@ -11,6 +11,12 @@ $msgSysNameVersion = SYSTEM_NAME.' - versão '.SYSTEM_VERSION;
 </head>
 <body>
     <h2><?php echo($msgSysNameVersion); ?></h2>
+    <li>versão 1.4.0</li>
+        <ul>
+            <li>Criação de relatórios: Produtos, Clientes e Itens Mais Vendidos</li>
+            <li>Acréscimo do campo observação do cliente no relatório de pedido de venda</li>
+            <li>Correções de problema de perfomance dos relatórios</li>
+        </ul>
     <li>versão 1.3.0</li>
         <ul>
             <li>Cadastro dos horários de verão</li>
